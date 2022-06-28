29bcad917482139390ce7463fa12456c2d1e58c5
darbinis <loginui888@gmail.com>
2022-06-28 15:57:49 +0300

    added styling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -58,5 +58,13 @@ export const Wrapper = styled.div`
         background: linear-gradient(180deg, #fff, #ffcc91);
         border: 2px solid #d38558;
         box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        height: 40px;
+        width: 140px;
+        margin: 20px 0;
+    }
+
+    .start {
+        max-width: 200px;
     }
 `
